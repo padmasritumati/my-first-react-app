42af0cecb402e678be8c0147a5f5ae49f96301ef
padmasritumati <tumatipadmasri98@gmail.com>
2020-06-08 17:43:19 +0200

    playing around with react components

---
+++ src/components/Title.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Title() {
+  return (
+    <div>
+      <h1>hi there :), how is it going ?</h1>
+      <p>thanks for coffee</p>
+    </div>
+  );
+}
